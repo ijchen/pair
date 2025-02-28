bdc8fbe8b992bb52381843c016c8e0e42b88cc39
Isaac Chen <isaacjchen1@gmail.com>
2025-02-28 16:50:09 -0500

    updated drop tests to check drop order

--- tests/drop.rs
+++ tests/drop.rs
@@ -3,51 +3,61 @@ use std::{cell::RefCell, convert::Infallible, rc::Rc};
 use pair::{HasDependent, Owner, Pair};
 
 #[derive(Debug)]
-struct OnDrop(Rc<RefCell<bool>>);
-impl Drop for OnDrop {
+struct OnDrop<T> {
+    value: Rc<RefCell<T>>,
+    f: fn(&mut T),
+}
+impl<T> Drop for OnDrop<T> {
     fn drop(&mut self) {
-        *self.0.borrow_mut() = true;
+        (self.f)(&mut self.value.borrow_mut())
     }
 }
 
-struct OnDropDep(Rc<RefCell<bool>>);
-impl Drop for OnDropDep {
+struct OnDropDep<T> {
+    value: Rc<RefCell<T>>,
+    f: fn(&mut T),
+}
+impl<T> Drop for OnDropDep<T> {
     fn drop(&mut self) {
-        *self.0.borrow_mut() = true;
+        (self.f)(&mut self.value.borrow_mut())
     }
 }
-impl<'owner> HasDependent<'owner> for OnDrop {
-    type Dependent = OnDropDep;
+impl<'owner, T> HasDependent<'owner> for OnDrop<T> {
+    type Dependent = OnDropDep<T>;
 }
 
-impl Owner for OnDrop {
-    type Context<'a> = Rc<RefCell<bool>>;
+impl<T> Owner for OnDrop<T> {
+    type Context<'a> = (Rc<RefCell<T>>, fn(&mut T));
     type Err = Infallible;
 
     fn make_dependent(
         &self,
         context: Self::Context<'_>,
     ) -> Result<<Self as HasDependent<'_>>::Dependent, Self::Err> {
-        Ok(OnDropDep(context))
+        Ok(OnDropDep {
+            value: context.0,
+            f: context.1,
+        })
     }
 }
 
 #[test]
 fn both_drops_called() {
-    let owner_drop_called = Rc::new(RefCell::new(false));
-    let dep_drop_called = Rc::new(RefCell::new(false));
+    // Using a common Vec instead of two bools, since we care about drop order
+    let drops_called = Rc::new(RefCell::new(Vec::new()));
     let pair = Pair::new_with_context(
-        OnDrop(Rc::clone(&owner_drop_called)),
-        Rc::clone(&dep_drop_called),
+        OnDrop {
+            value: Rc::clone(&drops_called),
+            f: |v| v.push("owner"),
+        },
+        (Rc::clone(&drops_called), |v| v.push("dep")),
     );
 
-    assert!(!*owner_drop_called.borrow());
-    assert!(!*dep_drop_called.borrow());
+    assert_eq!(*drops_called.borrow(), [] as [&str; 0]);
 
     drop(pair);
 
-    assert!(*owner_drop_called.borrow());
-    assert!(*dep_drop_called.borrow());
+    assert_eq!(*drops_called.borrow(), ["dep", "owner"]);
 }
 
 #[test]
@@ -55,8 +65,11 @@ fn dep_drop_called_on_into_owner() {
     let owner_drop_called = Rc::new(RefCell::new(false));
     let dep_drop_called = Rc::new(RefCell::new(false));
     let pair = Pair::new_with_context(
-        OnDrop(Rc::clone(&owner_drop_called)),
-        Rc::clone(&dep_drop_called),
+        OnDrop {
+            value: Rc::clone(&owner_drop_called),
+            f: |d| *d = true,
+        },
+        (Rc::clone(&dep_drop_called), |d| *d = true),
     );
 
     assert!(!*owner_drop_called.borrow());
