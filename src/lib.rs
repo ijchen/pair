bd80a3d27a5afe00047ee702778bd9b5ea086380
Isaac Chen <isaacjchen1@gmail.com>
2025-02-28 21:24:37 -0500

    updated test to-do documentation

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,3 @@ mod pair;
 
 pub use owner::{HasDependent, Owner};
 pub use pair::Pair;
-
-// TODO:
-// - Extensive testing, including:
-//   - Property-based testing
-//   - Some kind of "type level fuzzing"??
-//   - Test against known weird cases, like:
-//     - Types with all different kinds of variances
-//     - Weird drop impls (including "Oisann" types (jonhoo reference))
-//     - Impure / weird Deref impls
-//     - Interior mutable types
-//   - https://docs.rs/trybuild test cases demonstrating that misuses of the
-//     API don't compile
-//   - All under MIRI
-// - Soundness audits by experienced dark arts rustaceans
