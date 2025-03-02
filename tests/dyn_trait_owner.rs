a9b3a39d6fc3ef53bace24ab55f58e9f440ed30d
Isaac Chen <isaacjchen1@gmail.com>
2025-03-02 17:53:44 -0500

    added dynamic trait owner test

---
+++ tests/dyn_trait_owner.rs
@@ -0,0 +1,43 @@
+use std::convert::Infallible;
+
+use pair::{HasDependent, Owner, Pair};
+
+trait MyTrait {
+    fn get(&self) -> &i32;
+}
+
+struct MyConcrete(i32);
+
+impl MyTrait for MyConcrete {
+    fn get(&self) -> &i32 {
+        &self.0
+    }
+}
+
+impl<'owner> HasDependent<'owner> for dyn MyTrait {
+    type Dependent = &'owner i32;
+}
+impl Owner for dyn MyTrait {
+    type Context<'a> = ();
+    type Err = Infallible;
+
+    fn make_dependent<'owner>(
+        &'owner self,
+        (): Self::Context<'_>,
+    ) -> Result<<Self as HasDependent<'owner>>::Dependent, Self::Err> {
+        Ok(self.get())
+    }
+}
+
+#[test]
+fn dyn_trait_owner() {
+    let pair = Pair::new_from_box(Box::new(MyConcrete(69)) as Box<dyn MyTrait>);
+    let owner: &dyn MyTrait = pair.get_owner();
+    let dep: &i32 = pair.with_dependent(|dep| dep);
+
+    assert_eq!(owner.get(), &69);
+    assert_eq!(dep, &69);
+
+    let owner: Box<dyn MyTrait> = pair.into_boxed_owner();
+    assert_eq!(owner.get(), &69);
+}
